e3695091463dad4444f19056cd5834605743e2d2
Jesse Lang <jesse@jesselang.com>
2012-10-05 13:33:08 -0500

    Move create_file to it's own object so it can be included in dvsource-dvgrab.

---
+++ src/create_file.h
@@ -0,0 +1,21 @@
+// Copyright 2007-2008 Ben Hutchings.
+// Copyright 2008 Petter Reinholdtsen.
+// See the file "COPYING" for licence details.
+
+// File utility functions.
+
+#ifndef DVSWITCH_CREATE_FILE_H
+#define DVSWITCH_CREATE_FILE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int create_file(const char * format, char ** name);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* !defined(DVSWITCH_CREATE_FILE_H) */
+
--- src/dvsink-files.c
+++ src/dvsink-files.c
@@ -22,6 +22,7 @@
 #include "dif.h"
 #include "protocol.h"
 #include "socket.h"
+#include "create_file.h"
 
 static struct option options[] = {
     {"host",   1, NULL, 'h'},
@@ -64,85 +65,6 @@ Usage: %s [-h HOST] [-p PORT] [NAME-FORMAT]\n",
 struct transfer_params {
     int            sock;
 };
-
-static int create_file(const char * format, char ** name)
-{
-    time_t now;
-    struct tm now_local;
-    size_t name_buf_len = 200, name_len;
-    char * name_buf = 0;
-    int file;
-
-    now = time(0);
-    localtime_r(&now, &now_local);
-
-    // Allocate a name buffer and generate the name in it, leaving room
-    // for a suffix.
-    for (;;)
-    {
-	name_buf = realloc(name_buf, name_buf_len);
-	if (!name_buf)
-	{
-	    perror("realloc");
-	    exit(1);
-	}
-	name_len = strftime(name_buf, name_buf_len - 20,
-			    format, &now_local);
-	if (name_len > 0)
-	    break;
-
-	// Try a bigger buffer.
-	name_buf_len *= 2;
-    }
-
-    // Add ".dv" extension if missing.  Add distinguishing
-    // number before it if necessary to avoid collision.
-    // Create parent directories as necessary.
-    int suffix_num = 0;
-    if (name_len <= 3 || strcmp(name_buf + name_len - 3, ".dv") != 0)
-	strcpy(name_buf + name_len, ".dv");
-    else
-	name_len -= 3;
-    for (;;)
-    {
-	file = open(name_buf, O_CREAT | O_EXCL | O_WRONLY, 0666);
-	if (file >= 0)
-	{
-	    *name = name_buf;
-	    return file;
-	}
-	else if (errno == EEXIST)
-	{
-	    // Name collision; try changing the suffix
-	    sprintf(name_buf + name_len, "-%d.dv", ++suffix_num);
-	}
-	else if (errno == ENOENT)
-	{
-	    // Parent directory missing
-	    char * p = name_buf + 1;
-	    while ((p = strchr(p, '/')))
-	    {
-		*p = 0;
-		if (mkdir(name_buf, 0777) < 0 && errno != EEXIST)
-		{
-		    fprintf(stderr, "ERROR: mkdir %s: %s\n",
-			    name_buf, strerror(errno));
-		    exit(1);
-		}
-		*p++ = '/';
-	    }
-	}
-	else
-	{
-	    fprintf(stderr, "ERROR: open %s: %s\n",
-		    name_buf, strerror(errno));
-	    exit(1);
-	}
-    }
-
-    *name = name_buf;
-    return file;
-}
 
 static ssize_t write_retry(int fd, const void * buf, size_t count)
 {
